acde579f3f4caa7c7f6602445f353b534abd3f7f
Ruan Aragão <aragao.ruan@gmail.com>
2020-06-23 17:45:49 -0300

    feat: Instalando unform

--- src/pages/SingUp/index.tsx
+++ src/pages/SingUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -9,18 +10,21 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SingUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
   return (
     <Container>
       <Background />
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu Cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
-
           <Input
             name="password"
             icon={FiLock}
@@ -29,7 +33,7 @@ const SingUp: React.FC = () => {
           />
 
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
 
         <a href="login">
           <FiArrowLeft />
