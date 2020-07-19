6bac08910deb9ec5d24071c9315965d29eaec9a9
Ruan Aragao <aragao.ruan@gmail.com>
2020-07-19 16:35:47 -0300

    feat: Criando hook useAuth

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,31 +1,66 @@
-import React, { createContext, useCallback } from 'react';
+import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
+interface AuthState {
+  token: string;
+  user: User;
+}
 interface SingInCredentials {
   email: string;
   password: string;
 }
-
-interface AuthContextState {
-  name: string;
+interface AuthContextData {
+  user: User;
   singIn(credentials: SingInCredentials): Promise<void>;
 }
 
-export const AuthContext = createContext({} as AuthContextState);
+const AuthContext = createContext({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@goBarber:token');
+    const user = localStorage.getItem('@goBarber:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+
   const singIn = useCallback(async ({ email, password }) => {
     const response = await api.post('sessions', {
       email,
       password,
     });
 
-    console.log(response.data);
+    const { token, user } = response.data;
+
+    localStorage.setItem('@goBarber:token', token);
+    localStorage.setItem('@goBarber:user', JSON.stringify(user));
+
+    setData({ token, user });
   }, []);
 
   return (
-    <AuthContext.Provider value={{ name: 'Ruan', singIn }}>
+    <AuthContext.Provider value={{ user: data.user, singIn }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
+export function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
