15804059ef5bebb20511280cd0fdfa86e4a74d2b
Ruan Aragao <aragao.ruan@gmail.com>
2020-07-19 20:31:35 -0300

    feat: Rotas autenticadas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SingIn from '../pages/SingIn';
 import SingUp from '../pages/SingUp';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component={SingIn} />
       <Route path="/singup" component={SingUp} />
+
+      <Route path="/dashboard" isPrivate component={Dashboard} />
     </Switch>
   );
 };
